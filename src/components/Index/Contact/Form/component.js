c7fedc5039123e63e5545c7f117d16bca63a314c
Johan Soulet <johan.soulet@gmail.com>
2019-12-22 18:55:05 +0100

    feat(Form): add Netlify attr on contact form

--- src/components/Index/Contact/Form/component.js
+++ src/components/Index/Contact/Form/component.js
@@ -4,7 +4,7 @@ import { useIntl } from 'react-intl'
 const ContactForm = () => {
   const { formatMessage } = useIntl()
   return (
-    <form method="post" action="#" netlify>
+    <form method="post" action="#" data-netlify="true">
       <div className="row gtr-uniform">
         <div className="col-6 col-12-xsmall">
           <input
